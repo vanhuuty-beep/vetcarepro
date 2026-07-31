e2ae4da69cdbc2f428b4bf87ca6734f05d38e56d
vanhuuty-beep <vanhuuty@gmail.com>
2026-07-31 14:37:06 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-    SUPABASE_URL: "https://glbbycylllulafzrzfcz.supabase.co",
-    SUPABASE_KEY: "sb_publishable_-T8VmENvk62ZD2Ao4hJa1g_mC29Zftk"
+    // Nó sẽ lấy giá trị từ Vercel, nếu không có thì lấy giá trị mặc định bên dưới
+    SUPABASE_URL: window.ENV_SUPABASE_URL || "https://glbbycylllulafzrzfcz.supabase.co",
+    SUPABASE_KEY: window.ENV_SUPABASE_KEY || "sb_publishable_-T8VmENvk62ZD2Ao4hJa1g_mC29Zftk"
 };
